38db41d71976c9f0441810789949f2f63160fd6c
zhen <770226915@qq.com>
2022-12-01 11:34:27 +0800

    init

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,38 @@
 import { defineConfig } from "vitepress";
 
 export default defineConfig({
-  title: "vue111",
-  description: "博客11",
+  title: "进击的少年",
+  description: "进击的少年",
+  themeConfig: {
+    sidebar: [
+      {
+        text: "JavaScript",
+        collapsible: true,
+        items: [{ text: "promise", link: "/js/promise" }],
+      },
+      {
+        text: "TypeScript",
+        collapsible: true,
+        items: [
+          { text: "策略模式", link: "/ts/stragegy" },
+          { text: "命令模式", link: "/ts/command" },
+        ],
+      },
+      {
+        text: "Html",
+        collapsible: true,
+        items: [{ text: "html", link: "/html/" }],
+      },
+      {
+        text: "Css",
+        collapsible: true,
+        items: [{ text: "css", link: "/css/" }],
+      },
+      {
+        text: "Rust",
+        collapsible: true,
+        items: [{ text: "rust", link: "/rust/" }],
+      },
+    ],
+  },
 });
